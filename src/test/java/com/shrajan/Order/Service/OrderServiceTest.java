d57d979c49b7ed3f052c582db5d9c7fcc55e55f5
Shrajan <jainshrajan482@gmail.com>
2025-03-29 17:55:12 +0530

    added testcases for service using junit and mockito

--- src/test/java/com/shrajan/Order/Service/OrderServiceTest.java
+++ src/test/java/com/shrajan/Order/Service/OrderServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
