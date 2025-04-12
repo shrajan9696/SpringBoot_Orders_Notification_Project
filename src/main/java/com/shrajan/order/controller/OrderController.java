25d22f5d58b629f35ee584606c53a093b78cb7ae
Shrajan <jainshrajan482@gmail.com>
2025-04-12 16:00:33 +0530

    retsored to original

--- src/main/java/com/shrajan/order/controller/OrderController.java
+++ src/main/java/com/shrajan/order/controller/OrderController.java
@@ -22,7 +22,6 @@ public class OrderController {
     private final OrderService orderService;
     Logger logger = Logger.getLogger(getClass().getName());
 
-    // in future we will be using Kafka message broker
     public OrderController(WebClient.Builder webClientBuilder, OrderService orderService) {
         this.webClientBuilder = webClientBuilder;
         this.orderService = orderService;
